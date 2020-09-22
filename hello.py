92e5de006f24ceb322f6b57311bfaa8b62bc0943
RLKhoury <rakanlkhoury@gmail.com>
2020-09-21 22:01:54 -0400

    Added user endpoint with dynamic argument.

--- hello.py
+++ hello.py
@@ -3,7 +3,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hellow World!</h1>'
+    return '<h1>Hello, World!</h1>'
+
+@app.route('/user/<name>')
+def user(name):
+    return f"<h1>Hello, {name}!</h1>"
+
     
 if __name__ == '__main__':
     app.run(debug=True)
+    
